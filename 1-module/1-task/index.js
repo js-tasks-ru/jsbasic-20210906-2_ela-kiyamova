ac47aa427eb2a7d3f3addbdc0a8a9bf5e0f872b9
Kiamova Elvira <ela.kiyamova@yandex.ru>
2021-09-09 17:19:29 +0500

    решила задачу из модуля 0 и модуля 1 первую

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0) {return 1;}
+  let i = 1;
+  while (n > 0) {i *= n--;}
+  return i;
 }
+
+let number = +prompt('Введите число', '');
+alert(factorial(number));
+
+
